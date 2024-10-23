22968104daf91a0dbe439443615cb71276a27ba9
xxSkylineline <grayfx86@gmail.com>
2024-10-23 07:26:12 -0300

    agregando funcion de peticiones
    Esta feature se agrego para llevar a cabo una mayor legibilidad en la función principal y al mismo tiempo para que se pueda reutilizar dicha funcion

--- src/fetchData.js
+++ src/fetchData.js
@@ -1,3 +1,13 @@
+export async function url_pokeapi(url){
+   try{
+   const res = await fetch(url);
+   const data = await res.json();
+   return data
+   }
+   catch(error){
+      console.error('Error al realizar la solicitud', error)
+   }
+}
 
 export async function traerFotoPokemon(nombrePokemon){
      const url_nombrePokemon =`https://pokeapi.co/api/v2/pokemon/${nombrePokemon}`
@@ -10,3 +20,4 @@ export async function traerFotoPokemon(nombrePokemon){
         console.error('no pudo devolver la imagen ', error)
      }
 }
+
