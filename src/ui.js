683d7bc82a214ef715edca40d917941135439a5e
xxSkylineline <grayfx86@gmail.com>
2024-10-23 07:36:07 -0300

    Reorganización de la funcion
    Se optó para que la función sea mas legible.

--- src/ui.js
+++ src/ui.js
@@ -1,30 +1,35 @@
-export function crearTarjetaPokemon(nombrePokemon, indice, fotoPokemon){
-    let $tarjetaPokemon = document.querySelector('#tarjeta-pokemon');
-    const $cuadroPokemon = document.createElement('div');
-    const $tarjetaTitulo = document.createElement('h5');
-    const $imagenTarjeta = document.createElement('img');
-    
-    $cuadroPokemon.setAttribute('class', 'card');
-    $cuadroPokemon.style.height = "210px";
-    $cuadroPokemon.style.width = "210px";
-    $tarjetaPokemon.appendChild($cuadroPokemon);
-    
-    const $cuerpoTarjeta = document.querySelectorAll('.card')[indice];
-    $imagenTarjeta.setAttribute('class', 'card-img-top');
-    $imagenTarjeta.setAttribute('src', fotoPokemon)
-    $imagenTarjeta.dataset.nombrePokemon = nombrePokemon
-    $cuerpoTarjeta.appendChild($imagenTarjeta);
-    
-    const $nuevoCuadro = document.createElement('div')
-    $nuevoCuadro.setAttribute('class', 'card-body');
+export function crearTarjetaPokemon(nombrePokemon, indice, fotoPokemon) {
 
-    $cuerpoTarjeta.appendChild($nuevoCuadro);
+  const $tarjetaPokemon = document.querySelector("#tarjeta-pokemon");
+  const $cuadroPokemon = document.createElement("div");
+  $cuadroPokemon.setAttribute("class", "card");
+  $cuadroPokemon.style.height = "210px";
+  $cuadroPokemon.style.width = "210px";
+  $tarjetaPokemon.appendChild($cuadroPokemon);
+  
+  const $cuerpoTarjeta = document.querySelectorAll(".card")[indice];
+  const $imagenTarjeta = document.createElement("img");
+  $imagenTarjeta.setAttribute("class", "card-img-top");
+  $imagenTarjeta.setAttribute("src", fotoPokemon);
+  $imagenTarjeta.dataset.nombrePokemon = nombrePokemon;
+  $cuerpoTarjeta.appendChild($imagenTarjeta);
 
-    const $tarjetaTexto = document.querySelectorAll('.card-body')[indice];
+  const $nuevoCuadro = document.createElement("div");
+  $nuevoCuadro.setAttribute("class", "card-body");
+  $cuerpoTarjeta.appendChild($nuevoCuadro);
 
-    $tarjetaTitulo.setAttribute('class', 'card-text');
+  const $tarjetaTexto = document.querySelectorAll(".card-body")[indice];
+  const $tarjetaTitulo = document.createElement("p");
+  $tarjetaTitulo.setAttribute("class", "card-text");
 
-    $tarjetaTexto.appendChild($tarjetaTitulo)
-    $tarjetaTitulo.textContent = nombrePokemon
+  $tarjetaTexto.appendChild($tarjetaTitulo);
+  $tarjetaTitulo.textContent = nombrePokemon;
+
+  const $botonPokemon = document.createElement('a')
+  $botonPokemon.setAttribute('class','btn btn-primary');
+  $botonPokemon.setAttribute('href', '#');
+  $botonPokemon.textContent = 'Ir al pokemon'
+  $tarjetaTexto.appendChild($botonPokemon)
+  ;
 }
 
