48ffd3e03d74f912c9d39104d720bdaf50887112
xxSkylineline <grayfx86@gmail.com>
2024-10-29 15:29:22 -0300

    Refactorizacion de function tarjetaPokemon

--- src/ui.js
+++ src/ui.js
@@ -1,40 +1,29 @@
-export function crearTarjetaPokemon(nombrePokemon, indice, fotoPokemon) {
+export function crearTarjetaPokemon(nombrePokemon, indice) {
 
   const $tarjetaPokemon = document.querySelector("#tarjeta-pokemon");
-  const $cuadroPokemon = document.createElement("div");
-  $cuadroPokemon.setAttribute("class", "card");
-  $cuadroPokemon.style.height = "210px";
-  $cuadroPokemon.style.width = "210px";
+  let $cuadroPokemon = crearTarjeta() 
   $tarjetaPokemon.appendChild($cuadroPokemon);
   
   const $cuerpoTarjeta = document.querySelectorAll(".card")[indice];
-  const $imagenTarjeta = document.createElement("img");
-  $imagenTarjeta.setAttribute("class", "card-img-top");
-  $imagenTarjeta.setAttribute("src", fotoPokemon);
-  $imagenTarjeta.dataset.nombrePokemon = nombrePokemon;
-  $cuerpoTarjeta.appendChild($imagenTarjeta);
-
+  
   const $nuevoCuadro = document.createElement("div");
   $nuevoCuadro.setAttribute("class", "card-body");
   $cuerpoTarjeta.appendChild($nuevoCuadro);
 
   const $tarjetaTexto = document.querySelectorAll(".card-body")[indice];
-  const $tarjetaTitulo = document.createElement("p");
-  $tarjetaTitulo.setAttribute("class", "card-text");
-
+  const $tarjetaTitulo = crearTarjetaTitulo(nombrePokemon)
   $tarjetaTexto.appendChild($tarjetaTitulo);
-  $tarjetaTitulo.textContent = nombrePokemon;
-
+  
   const $botonPokemon = document.createElement('a')
-  $botonPokemon.setAttribute('class','btn btn-primary');
-  $botonPokemon.setAttribute('href', '#');
-  $botonPokemon.textContent = 'Ir al pokemon'
+  $botonPokemon.setAttribute('class','btn btn-primary boton-estadisticas');
+  $botonPokemon.dataset.nombrePokemon = nombrePokemon
+  $botonPokemon.textContent = 'Ver estadísticas'
   $tarjetaTexto.appendChild($botonPokemon)
   ;
 }
 
-export async function crearPaginas(paginaSiguiente){
-    const link = await paginaSiguiente
+export async function crearPagina(paginaSiguiente, numeroDePagina){
+    const link = paginaSiguiente
     const $contenedorDePaginacion = document.createElement('li')
     $contenedorDePaginacion.setAttribute('class','page-item');
 
