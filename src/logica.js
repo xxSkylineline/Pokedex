89393c288a89ed95993f1727ff0165e45e920917
xxSkylineline <grayfx86@gmail.com>
2024-10-31 23:18:30 -0300

    creando las funciones del archivo de logica

--- src/logica.js
+++ src/logica.js
@@ -5,3 +5,21 @@ import {
   crearTarjetaEstadisticasPokemon,
 } from "./ui.js";
 
+const url_pokeApi = "https://pokeapi.co/api/v2/pokemon/";
+
+export async function mostrarPokemones(link) {
+  document.querySelector("#tarjeta-pokemon").textContent = "";
+
+  try {
+    const res = await traerDatosPokeapi(link);
+
+    console.log(res.results.length);
+    for (let i = 0; i < Object.keys(res.results).length; i++) {
+      const nombrePokemon = res.results[i].name;
+
+      crearTarjetaPokemon(nombrePokemon, i);
+    }
+  } catch (error) {
+    console.error("Error al requerir una respuesta:", error);
+  }
+}
