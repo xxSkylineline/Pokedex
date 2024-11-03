a44cc461d0a6ad865dfb807791e4df45c22ee101
xxSkylineline <grayfx86@gmail.com>
2024-11-02 23:42:40 -0300

    refactorizado de funcion
    Había creado dos funciones que prácticamente buscaban el mismo recurso pero se llego a la conclusión que con una sola función era suficiente para poder manipular los datos como las imagenes del pokemon

--- src/fetchData.js
+++ src/fetchData.js
@@ -1,23 +1,9 @@
-export async function url_pokeapi(url){
-   try{
-   const res = await fetch(url);
-   const data = await res.json();
-   return data
-   }
-   catch(error){
-      console.error('Error al realizar la solicitud', error)
-   }
-}
-
-export async function traerFotoPokemon(nombrePokemon){
-     const url_nombrePokemon =`https://pokeapi.co/api/v2/pokemon/${nombrePokemon}`
-    
-     try{
-        const res = await fetch(url_nombrePokemon);
-        const data = await res.json();
-        return data.sprites.other.dream_world.front_default;
-     } catch (error){
-        console.error('no pudo devolver la imagen ', error)
-     }
-}
-
+export async function traerDatosPokeapi(url) {
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.error("Error al realizar la solicitud", error);
+  }
+};
